95e75d83704e9d75e19d9f57ca8fa171c8bc9cb5
Simon Andersson <108905796+Grymto@users.noreply.github.com>
2022-12-23 19:50:04 +0100

    Add files via upload

---
+++ Assaignment 1_5 Simon/script/script.js
@@ -0,0 +1,32 @@
+let age = 17
+let Vip = true
+
+alert ("Assignment 1_5")
+
+
+
+console.log ("Club Exlusive");
+
+function click() {
+    age = 17
+    age = parseInt(document.getElementById("input1").value,10);
+    console.log(age)
+
+}
+  
+    if (Vip == true) {console.log ("Välkomen Vår Bästa kund!");
+    }
+
+    else {
+        console.log("Vill du bli medlem ?")
+    }
+    if (age >= 18 ) {
+        console.log ("Välkomen!");
+        }
+    
+        else {
+            console.log ("du är minderårig");
+        }
+
+    
+
